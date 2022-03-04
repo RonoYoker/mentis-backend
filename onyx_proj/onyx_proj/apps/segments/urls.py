9e86cc70a780fb44bf083e3a21e8475c3a77e607
Siddharth Sharma <siddharth.sharma@creditas.in>
2022-03-04 18:00:56 +0530

    url path changes

--- onyx_proj/onyx_proj/apps/segments/urls.py
+++ onyx_proj/onyx_proj/apps/segments/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("saveCustomSegment/", views.save_custom_segment),
-    path("getSegments/", views.get_all_segments),
-    path("fetchHeaders/", views.fetch_headers_by_segment_id),
-    path("headerCompatibilityCheck/", views.check_headers_compatibility)
+    path("save_custom_segment/", views.save_custom_segment),
+    path("get_segments/", views.get_all_segments),
+    path("fetch_headers/", views.fetch_headers_by_segment_id),
+    path("header_compatibility_check/", views.check_headers_compatibility)
 ]
