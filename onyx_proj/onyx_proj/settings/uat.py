afb9fc766b90ecbdd20e7f7ddbc8fcf434f582ed
Siddharth Sharma <siddharth.sharma@creditas.in>
2022-04-08 16:36:07 +0530

    adding KOTAK project to domain config

--- onyx_proj/onyx_proj/settings/uat.py
+++ onyx_proj/onyx_proj/settings/uat.py
@@ -27,6 +27,8 @@ DATABASES = {
 HYPERION_LOCAL_DOMAIN = {
     "TEST_TCL": "https://3.111.61.77/",  # https://tclctpay.tatacapital.com/hyperioncampaigntooldashboard
     "TEST_IBL": "http://3.108.170.118/",
+    "TEST_KOTAK": "http://uatpay.kotak.com/hyperioncampaigntooldashboard/",
     "IBL_Ethera": "http://m-prod-indus-hyp-2094481502.ap-south-1.elb.amazonaws.com/",
-    "TCL_Ethera": "http://m-prod-tatacapital-hyp-1407059969.ap-south-1.elb.amazonaws.com/"
+    "TCL_Ethera": "http://m-prod-tatacapital-hyp-1407059969.ap-south-1.elb.amazonaws.com/",
+    "KOTAK_Ethera": "https://pay.kotak.com/hyperioncampaigntooldashboard/",
 }
