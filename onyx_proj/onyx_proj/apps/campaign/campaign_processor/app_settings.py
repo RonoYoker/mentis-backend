04d3a7bdc6d29ba6e88471e107549ee38e9d9a91
ritik saini <ritik.saini@creditas.in>
2022-06-10 16:55:26 +0530

    changed min/max sched time

--- onyx_proj/onyx_proj/apps/campaign/campaign_processor/app_settings.py
+++ onyx_proj/onyx_proj/apps/campaign/campaign_processor/app_settings.py
@@ -10,13 +10,13 @@ CAMPAIGN_STATES={
 
 SCHEDULED_CAMPAIGN_TIME_RANGE_UTC = {
     "min" : {
-        "hour": 0,
+        "hour": 3,
         "min":30,
         "sec":0
     },
     "max" : {
-        "hour": 16,
-        "min":30,
+        "hour": 13,
+        "min":15,
         "sec":0
     }
 }
