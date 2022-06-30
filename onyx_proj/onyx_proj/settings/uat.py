9c19af846d99888742779e98b47e7027d2336adb
ritik saini <ritik.saini@creditas.in>
2022-06-30 17:15:13 +0530

    added cong for prl uat

--- onyx_proj/onyx_proj/settings/uat.py
+++ onyx_proj/onyx_proj/settings/uat.py
@@ -31,7 +31,8 @@ HYPERION_LOCAL_DOMAIN = {
     "IBL_Ethera": "http://m-prod-indus-hyp-2094481502.ap-south-1.elb.amazonaws.com/",
     "TCL_Ethera": "http://m-prod-tatacapital-hyp-1407059969.ap-south-1.elb.amazonaws.com/",
     "KOTAK_Ethera": "https://pay.kotak.com/",
-    "TEST_RBL": "http://uat-pay.rblbank.com/"
+    "TEST_RBL": "http://uat-pay.rblbank.com/",
+    "TEST_PRL": "http://15.207.224.106/"
 }
 
 CAMPAIGN_THRESHOLDS_PER_MINUTE = {
