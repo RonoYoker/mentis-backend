8cab4d6e7a34d1ca078948a661a225fa5bca8e4f
ritik saini <ritik.saini@creditas.in>
2022-08-05 12:47:11 +0530

    changed tcl/ibl uat domain

--- onyx_proj/onyx_proj/settings/uat.py
+++ onyx_proj/onyx_proj/settings/uat.py
@@ -24,8 +24,8 @@ DATABASES = {
 }
 
 HYPERION_LOCAL_DOMAIN = {
-    "TEST_TCL": "https://3.111.61.77/",  # https://tclctpay.tatacapital.com/hyperioncampaigntooldashboard
-    "TEST_IBL": "http://3.108.170.118/",
+    "TEST_TCL": "https://tclctpay.tatacapital.com/",  # https://tclctpay.tatacapital.com/hyperioncampaigntooldashboard
+    "TEST_IBL": "http://uatpay.indusind.com/",
     "TEST_KOTAK": "http://uatpay.kotak.com/",
     "IBL_Ethera": "http://m-prod-indus-hyp-2094481502.ap-south-1.elb.amazonaws.com/",
     "TCL_Ethera": "http://m-prod-tatacapital-hyp-1407059969.ap-south-1.elb.amazonaws.com/",
