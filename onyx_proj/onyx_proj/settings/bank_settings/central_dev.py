9257d20e10f7fc64d597f79b9a07959c7b3bfbfa
creditas-mohitlamba <100125452+creditas-mohitlamba@users.noreply.github.com>
2023-06-02 01:11:39 +0530

    Revert "Add telegram Class function (#268)" (#269)
    This reverts commit 0673944b1a78a2050ac9079db9d3f4a775659662.

--- onyx_proj/onyx_proj/settings/bank_settings/central_dev.py
+++ onyx_proj/onyx_proj/settings/bank_settings/central_dev.py
@@ -69,11 +69,3 @@ BCC_CAMPAIGN_DEACTIVATE_EMAIL_ID = []
 
 CC_LIST = []
 BCC_LIST = []
-
-VENDOR_CONFIG = {
-    "telegram": {
-        "API_URL": "http://m-stage-sandesh-elb-1821377603.ap-south-1.elb.amazonaws.com/api/send_communication",
-        "CLIENT": "HYPERION_CENTRAL",
-        "CONFIG_ID": "hyp_telegram_uat"
-    }
-}
