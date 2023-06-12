7c4d12e7a97787fc0789910a0f2528d542b6ca01
ritik saini <ritik.saini@creditas.in>
2023-06-12 23:46:00 +0530

    bug

--- onyx_proj/onyx_proj/orm_models/CED_Segment_model.py
+++ onyx_proj/onyx_proj/orm_models/CED_Segment_model.py
@@ -8,6 +8,7 @@ class CED_Segment(Base, Orm_helper):
     title = Column("Title", String)
     project_id = Column("ProjectId", String)
     data_id = Column("DataId", String)
+    segment_builder_id = Column("SegmentBuilderId", String)
     include_all = Column("IncludeAll", Integer)
     sql_query = Column("SqlQuery", String)
     campaign_sql_query = Column("CampaignSqlQuery", String)
@@ -24,9 +25,9 @@ class CED_Segment(Base, Orm_helper):
     is_deleted = Column("IsDeleted",  Integer, default=0)
     ever_scheduled = Column("EverScheduled", Integer, default=0)
     last_campaign_date = Column("LastCampaignDate", TIMESTAMP)
-    creation_date = Column("CreationDate", TIMESTAMP)
-    updation_date = Column("UpdationDate", TIMESTAMP,
-                           server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+    creation_date = Column("CreationDate", TIMESTAMP,default="CURRENT_TIMESTAMP",server_default=func.now())
+    updation_date = Column("UpdationDate", TIMESTAMP,default="CURRENT_TIMESTAMP",
+                           server_default=func.now())
     history_id = Column("HistoryId", String)
     extra = Column("Extra", String)
     type = Column("Type", String)
