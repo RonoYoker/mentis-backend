412752fb474e8327b134de44d3b758d2ef5a83dc
Siddharth Sharma <siddharth.sharma@creditas.in>
2023-09-07 16:12:00 +0530

    changing local creds for hdb

--- onyx_proj/onyx_proj/settings/bank_settings/hdb_prod.py
+++ onyx_proj/onyx_proj/settings/bank_settings/hdb_prod.py
@@ -44,4 +44,4 @@ ONYX_CENTRAL_AUTH_TOKEN = "ACDB144B-5CAF-473F-9B1C-FEF493AA6865"
 AWS_REGION = "ap-south-1"
 AWS_ACCOUNTID = 972198454828
 SNS_SEGMENT_EVALUATOR = "PROD_HYP_DK_Campaign_Segment_Evaluator"
-QUERY_EXECUTION_JOB_BUCKET = "ibl-prod-async-query-execution-response"
+QUERY_EXECUTION_JOB_BUCKET = "hdb-prod-async-query-execution-response"
