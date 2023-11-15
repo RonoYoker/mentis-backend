f0535f33a6b2e069ef45cc60386847656d28d335
ritik saini <ritik.saini@creditas.in>
2023-11-15 17:00:11 +0530

    add log

--- onyx_proj/onyx_proj/models/CED_WHATSAPPResponse_model.py
+++ onyx_proj/onyx_proj/models/CED_WHATSAPPResponse_model.py
@@ -16,7 +16,7 @@ class CEDWHATSAPPResponse:
     def check_campaign_click_and_delivery_data(self, campaign_ids: [], mobile_number, click=False):
         campaign_id_tuple = f"('{campaign_ids[0]}')" if len(campaign_ids) == 1 else tuple(campaign_ids)
         if click is False:
-            query = f'SELECT CampaignId, Status, UpdateDate from CED_WhatsAppResponse where CampaignId in {campaign_id_tuple} and EnMobileNumber = {mobile_number} and TestCampaign = 1 ORDER BY CampaignId DESC LIMIT 1'
+            query = f"SELECT CampaignId, Status, UpdateDate from CED_WhatsAppResponse where CampaignId in {campaign_id_tuple} and EnMobileNumber = '{mobile_number}' and TestCampaign = 1 ORDER BY CampaignId DESC LIMIT 1"
         else:
             query = f"SELECT wr.CampaignId, wr.ShortUUID, wr.Status, wr.UpdateDate, wt.UpdationDate from CED_WhatsAppResponse wr left join CED_SMSClickTracker wt on wr.ShortUUID = wt.ShortUUID where wr.TestCampaign = 1 and wr.CampaignId in {campaign_id_tuple} and wr.EnMobileNumber = '{mobile_number}' ORDER BY wr.CampaignId DESC LIMIT 1"
         return fetch_all(self.curr, query)
