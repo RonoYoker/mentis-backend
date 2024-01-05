4dd86e057531713318d2c043f8463dc28de2e46b
Siddharth Sharma <siddharth.sharma@creditas.in>
2024-01-05 15:50:35 +0530

    changing ibl_uat and ybl_uat for testing

--- onyx_proj/onyx_proj/settings/bank_settings/ibl_uat.py
+++ onyx_proj/onyx_proj/settings/bank_settings/ibl_uat.py
@@ -9,6 +9,7 @@ INFRA_CONF = fetch_secrets_from_secret_manager(secret_name, region_name)
 
 DATABASES = INFRA_CONF["DATABASES"]
 
+# SEGMENT_AES_KEYS = INFRA_CONF["SEGMENT_AES_KEYS"]
 
 # *********** CELERY CONFIGURATION ********************************
 BROKER_URL = f"redis://{INFRA_CONF['BROKER_URL']}:6379/12"
@@ -17,6 +18,32 @@ CELERY_BROKER_URL = f"redis://{INFRA_CONF['BROKER_URL']}:6379/12"
 CELERY_RESULT_BACKEND = f"redis://{INFRA_CONF['BROKER_URL']}:6379/12"
 # *********** CELERY CONFIGURATION FIN ********************************
 
+
+CELERY_IMPORTS = [
+    'onyx_proj.celery_app.tasks',
+    'onyx_proj.apps.async_task_invocation.async_tasks_processor',
+    'onyx_proj.apps.campaign.campaign_engagement_data.engagement_data_processor',
+    'onyx_proj.apps.campaign.views'
+]
+# CELERY_BEAT_SCHEDULE = CELERY_BEAT_SCHEDULE
+CELERY_IMPORTS = CELERY_IMPORTS
+# Celery application definition
+CELERY_APP_NAME = 'celery'
+CELERY_CREATE_MISSING_QUEUES = True
+# Do not store any async task return result, as we do not use them
+CELERY_IGNORE_RESULT = True
+
+# But store errors if any
+CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
+
+CELERYD_PREFETCH_MULTIPLIER = 1
+
+CELERY_ACCEPT_CONTENT = ['pickle', 'json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_TIMEZONE = 'UTC'
+CELERY_ENABLE_UTC = False
 
 CENTRAL_TO_LOCAL_ENCRYPTION_KEY = INFRA_CONF["CENTRAL_TO_LOCAL_ENCRYPTION_KEY"]
 UUID_ENCRYPTION_KEY = INFRA_CONF["UUID_ENCRYPTION_KEY"]
@@ -36,7 +63,7 @@ TEST_CAMPAIGN_DELIVERY_VALIDATION = {
 }
 
 # ONYX_DOMAIN = "https://onyx.hyperiontool.com" # prod
-ONYX_DOMAIN = "http://onyxuat.hyperiontool.com"  # uat
+ONYX_DOMAIN = "https://onyxuat.hyperiontool.com"  # uat
 # ONYX_DOMAIN = "http://127.0.0.1:8084" # dev
 
 ONYX_CENTRAL_AUTH_TOKEN = "348691BE-D2AA-4460-9A42-2B4A9FFD3296"
@@ -48,6 +75,13 @@ AWS_ACCOUNTID = INFRA_CONF["AWS_ACCOUNTID"]
 SNS_SEGMENT_EVALUATOR = "TEST_HYP_DK_Campaign_Segment_Evaluator"
 QUERY_EXECUTION_JOB_BUCKET = "ibl-uat-async-query-execution-response"
 
+SHORT_URL_BUCKET_CONFIG = {
+    "PEGASUS_DECAF_UAT": {
+        "MANDATORY_FIELDS": ["primary_key", "account_id", "channel", "unique_id"],
+        "SHORT_URL_BUCKET_UNIQUE_ID": "SHORTURL_1"
+    }
+}
 MKT_CLICKDATA_FLAG = True
+
 
 ETHERA_COMMONS_DOMAIN = "http://m-stage-ethera-commons-elb-61896516.ap-south-1.elb.amazonaws.com"
