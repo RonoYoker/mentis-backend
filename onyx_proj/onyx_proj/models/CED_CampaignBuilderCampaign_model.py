0565c6b6a9331d3aee2e47a8e578272b67b07ffc
Siddharth Sharma <siddharth.sharma@creditas.in>
2024-01-06 07:59:04 +0530

    changing query

--- onyx_proj/onyx_proj/models/CED_CampaignBuilderCampaign_model.py
+++ onyx_proj/onyx_proj/models/CED_CampaignBuilderCampaign_model.py
@@ -375,9 +375,10 @@ class CEDCampaignBuilderCampaign:
         return resp
 
     def bulk_update_segment_data_for_cbc_ids(self, cbc_ids: str, segment_data: dict):
-        query = f"""UPDATE CED_CampaignBuilderCampaign SET S3Path = '{segment_data["S3Path"]}', S3DataRefreshEndDate = '{segment_data["S3DataRefreshEndDate"]}', S3DataHeadersList = %s, S3DataRefreshStatus = '{segment_data["S3DataRefreshStatus"]} WHERE UniqueId IN (%s);""" % (segment_data["S3DataHeadersList"], cbc_ids)
+        query = f"""UPDATE CED_CampaignBuilderCampaign SET S3Path = '{segment_data["S3Path"]}', S3DataRefreshEndDate = '{segment_data["S3DataRefreshEndDate"]}', S3DataHeadersList = %s, S3DataRefreshStatus = '{segment_data["S3DataRefreshStatus"]} WHERE UniqueId IN (%s);"""
         print("query: ", query)
-        return execute_update_query(self.engine, query)
+        return update_table_row(self.engine, query, (segment_data["S3DataHeadersList"], cbc_ids))
+
 
     def get_recurring_details_json(self, cbc_id: str):
         query= f"""
