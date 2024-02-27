6c374fa4059e357b8ceb7923f8393f35b915c168
ritik saini <ritik.saini@creditas.in>
2024-02-27 14:26:47 +0530

    bug

--- onyx_proj/onyx_proj/apps/campaign/campaign_processor/test_campaign_processor.py
+++ onyx_proj/onyx_proj/apps/campaign/campaign_processor/test_campaign_processor.py
@@ -401,7 +401,7 @@ def decrypt_test_segment_data(data, headers, project_id):
     for record in data:
         record = {k.lower(): v for k, v in record.items()}
         for header in headers:
-            if header.get("encrypted", False) == True and record[header["headerName"].lower()] is not None and record[header["headerName"].lower()] == '\\N':
+            if header.get("encrypted", False) == True and record[header["headerName"].lower()] is not None and record[header["headerName"].lower()] != '\\N':
                 encrypted_values.append(record[header["headerName"].lower()])
         lower_case_data.append(record)
 
