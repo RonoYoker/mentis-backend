55b9401387836a4beb28bbab97c004178b98ba47
ritik <ritik.saini@creditas.in>
2024-09-20 21:31:47 +0530

    csrf

--- mentis_proj/mentis_proj/apps/user/views.py
+++ mentis_proj/mentis_proj/apps/user/views.py
@@ -5,8 +5,9 @@ from django.http import HttpResponse
 
 from mentis_proj.apps.user.user_data_processor import process_login_request, process_logout_request
 from mentis_proj.middlewares.HttpRequestInterceptor import Session
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def user_login(request):
     request_body = json.loads(request.body.decode("utf-8"))
     resp = process_login_request(request_body)
@@ -16,12 +17,14 @@ def user_login(request):
         return http_resp
     return  HttpResponse(json.dumps({"success":False}, default=str), content_type="application/json")
 
+@csrf_exempt
 def user_logout(request):
     session = Session()
     auth_token = session.user_session["auth_token"]
     resp =  process_logout_request(auth_token)
     return  HttpResponse(json.dumps(resp, default=str), content_type="application/json")
 
+@csrf_exempt
 def self(request):
     session = Session()
     user_session_details = session.user_session
