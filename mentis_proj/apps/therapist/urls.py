325dd796303c2b816e363154e68075837cd0fb7d
ritik <ritik.saini@creditas.in>
2024-10-24 01:34:22 +0530

    added Dockerfile

--- mentis_proj/apps/therapist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-from mentis_proj.apps.therapist.views import *
-
-urlpatterns = [
-    path("fetch-therapist/", fetch_therapist),
-    path("fetch-therapist/<int:therapist_id>/", fetch_therapist_details)
-]
-
--- mentis_proj/settings/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-if os.environ["CURR_ENV"].lower() == "prod":
-    from .prod import *
-else:
-    from .uat import *
