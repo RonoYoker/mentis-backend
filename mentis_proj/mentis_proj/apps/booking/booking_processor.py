0c7adb06135897113f4291e97823c823e5c1a43a
ritik <ritik.saini@creditas.in>
2025-01-21 23:18:47 +0530

    slots api changed

--- mentis_proj/mentis_proj/apps/booking/booking_processor.py
+++ mentis_proj/mentis_proj/apps/booking/booking_processor.py
@@ -6,39 +6,45 @@ from mentis_proj.apps.booking.db_helper import  Booking
 
 
 
-def fetch_therapist_slots(therapist_id,date,timeframe_mins):
+def fetch_therapist_slots(therapist_id,from_date,to_date,timeframe_mins):
     avail_slots = []
     therapist_det = fetch_therpaist_details(therapist_id)
     if therapist_det["success"] is False:
         return therapist_det
     avail_info = json.loads(therapist_det["data"]["availability_info"])
-    if date.strftime("%A") in avail_info["non_avail_days"]:
-        return {"success":False , "avail_slots":avail_slots}
-    starting_slot = datetime.strptime(avail_info["general_avail"]["start_time"],"%H:%M")
-    ending_slot = datetime.strptime(avail_info["general_avail"]["end_time"],"%H:%M")
-    all_slots = []
-    for x in range(0,100):
-        all_slots.append((starting_slot + timedelta(minutes=30*x)).time())
-        if (starting_slot + timedelta(minutes=30*(x-1))) == ending_slot:
-            break
-    non_avail_slots_data = Booking().fetch_therapist_slots(therapist_id,date)
-    if non_avail_slots_data["success"] is False:
-        return {"success":False , "avail_slots":avail_slots}
-    non_avail_slots = []
-    for slot in non_avail_slots_data["data"]:
-        if slot["end_time"] - slot["start_time"] == timedelta(minutes=60):
-            non_avail_slots.append(slot["start_time"].time())
-            non_avail_slots.append((slot["start_time"]+timedelta(minutes=30)).time())
-        else:
-            non_avail_slots.append(slot["start_time"].time())
-    remaining_slots = sorted(list(set(all_slots)-set(non_avail_slots)))
-    if timeframe_mins == 30:
-        avail_slots = [x.strftime("%H:%M") for x in remaining_slots]
-    elif timeframe_mins == 50:
-        for i in range(0,len(remaining_slots)-1):
-            if datetime.combine(datetime.now().date(),remaining_slots[i+1]) - datetime.combine(datetime.now().date(),remaining_slots[i]) == timedelta(minutes=30):
-                avail_slots.append(remaining_slots[i].strftime("%H:%M"))
-    return {"success": False, "avail_slots": avail_slots}
+    date = from_date
+    resp = {}
+    while date < to_date:
+
+        if date.strftime("%A") in avail_info["non_avail_days"]:
+            return {"success":False , "avail_slots":avail_slots}
+        starting_slot = datetime.strptime(avail_info["general_avail"]["start_time"],"%H:%M")
+        ending_slot = datetime.strptime(avail_info["general_avail"]["end_time"],"%H:%M")
+        all_slots = []
+        for x in range(0,100):
+            all_slots.append((starting_slot + timedelta(minutes=30*x)).time())
+            if (starting_slot + timedelta(minutes=30*(x-1))) == ending_slot:
+                break
+        non_avail_slots_data = Booking().fetch_therapist_slots(therapist_id,date)
+        if non_avail_slots_data["success"] is False:
+            return {"success":False , "avail_slots":avail_slots}
+        non_avail_slots = []
+        for slot in non_avail_slots_data["data"]:
+            if slot["end_time"] - slot["start_time"] == timedelta(minutes=60):
+                non_avail_slots.append(slot["start_time"].time())
+                non_avail_slots.append((slot["start_time"]+timedelta(minutes=30)).time())
+            else:
+                non_avail_slots.append(slot["start_time"].time())
+        remaining_slots = sorted(list(set(all_slots)-set(non_avail_slots)))
+        if timeframe_mins == 30:
+            avail_slots = [x.strftime("%H:%M") for x in remaining_slots]
+        elif timeframe_mins == 50:
+            for i in range(0,len(remaining_slots)-1):
+                if datetime.combine(datetime.now().date(),remaining_slots[i+1]) - datetime.combine(datetime.now().date(),remaining_slots[i]) == timedelta(minutes=30):
+                    avail_slots.append(remaining_slots[i].strftime("%H:%M"))
+
+        resp[date.strftime("%Y-%m-%d")]=avail_slots
+    return {"success": True, "avail_slots": resp}
 
 
 
--- mentis_proj/mentis_proj/apps/booking/views.py
+++ mentis_proj/mentis_proj/apps/booking/views.py
@@ -2,6 +2,8 @@ import json
 from datetime import datetime
 
 from django.http import HttpResponse
+
+from mentis_proj.exceptions.exceptions import ValidationFailedException
 from mentis_proj.apps.booking.booking_processor import fetch_therapist_slots
 from django.views.decorators.csrf import csrf_exempt
 
@@ -10,8 +12,12 @@ def fetch_therapist_avl_slots(request):
     request_body = json.loads(request.body.decode("utf-8"))
     therapist_id = request_body["therapist_id"]
     timeframe = request_body["timeframe"]
-    date = datetime.strptime(request_body["date"],"%Y-%m-%d")
-    resp = fetch_therapist_slots(therapist_id,date,timeframe)
+    curr_date = datetime.now()
+    from_date = datetime.strptime(request_body["from_date"],"%Y-%m-%d")
+    to_date = datetime.strptime(request_body["to_date"],"%Y-%m-%d")
+    if from_date >= to_date or from_date < curr_date or to_date < curr_date:
+        raise ValidationFailedException(reason="Invalid dates supplied")
+    resp = fetch_therapist_slots(therapist_id,from_date,to_date,timeframe)
     return HttpResponse(json.dumps(resp, default=str), content_type="application/json")
 
 
